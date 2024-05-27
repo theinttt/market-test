276c2afa49af6a3ec6d3557ecfcc70b48173a142
Theint Theint Thu <theinttheintthu29405@gmail.com>
2024-05-27 07:38:34 +0630

    second

--- Market/src/language/Korean.java
+++ Market/src/language/Korean.java
@@ -1,5 +1,24 @@
 package language;
 
 public class Korean {
-
+	
+	public void callmethod() {
+		greet();
+		showmenu();
+		
+	}
+	
+	public void greet() {
+		System.out.println("*******************");
+		System.out.println("******* 안녕 *******");
+		System.out.println("*******************");
+	}
+	
+	public void showmenu() {
+		System.out.println("     * 우리 시장의 품목 *         ");
+		System.out.println("     1. 과일                   ");
+		System.out.println("     2. 감기                   ");
+		System.out.println("     3. 바로 먹을 수 있는 식품     ");
+		System.out.println("     4. 야채                   ");
+	}
 }
--- Market/src/language/Myanmar.java
+++ Market/src/language/Myanmar.java
@@ -1,6 +1,11 @@
 package language;
 
+import java.util.Scanner;
+
 public class Myanmar {
+	
+	Scanner sc = new Scanner(System.in);
+	int category;
 	
 	public void callmethod() {
 		greet();
@@ -16,10 +21,27 @@ public class Myanmar {
 	
 	public void showmenu() {
 		System.out.println("*ကျွန်ုပ်တို့ Market တွင်ရှိသောပစ္စည်းများ*");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+		System.out.println("     ၁. အသီးအနှံများ           ");
+		System.out.println("     ၂. အအေးများ             ");
+		System.out.println("     ၃. အသင့်စားအစားအစာများ     ");
+		System.out.println("     ၄. ဟင်းသီးဟင်းရွက်များ       ");
+		
+		System.out.println("မိမိဝယ်ယူလိုသောအမျိူးအစားကိုရွေးပါ");
+		category = sc.nextInt();
+		
+		if (category == 1) {
+			fruit();
+		}
 	}
 
+	public void fruit() {
+		System.out.println("*   အသီးအနှံများ   *");
+		System.out.println("***************");
+		System.out.println("*   ပန်းသီး       *");
+		System.out.println("*   နာနတ်သီး      *");
+		System.out.println("*   လိမ္မော်သီး      *");
+		System.out.println("*   ဖရဲသီး        *");
+		System.out.println("*   စတော်ဘယ်ရီ    *");
+		System.out.println("*   စပျစ်သီး       *");
+	}
 }
